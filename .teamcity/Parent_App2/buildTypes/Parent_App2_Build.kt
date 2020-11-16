cbe1f67a1a5166f2f0a4e7b013da1af7451c4714
Nguyen <nguyenxndaidev@gmail.com>
2020-11-16 20:17:09 +0100

    Update Parent_App2_Build.kt

--- .teamcity/Parent_App2/buildTypes/Parent_App2_Build.kt
+++ .teamcity/Parent_App2/buildTypes/Parent_App2_Build.kt
@@ -5,7 +5,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 
 object Parent_App2_Build : BuildType({
     uuid = "b8a67d62-1718-4c25-8661-52ff74293c58"
-    name = "clean compile"
+    name = "clean then compile"
 
     vcs {
         root(Parent_App2.vcsRoots.Parent_App2_SourceApp2)
