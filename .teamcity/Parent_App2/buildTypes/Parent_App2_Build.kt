77609acb3bd744e4a7f7ffd379d59be647e1c875
Xuan Dai Nguyen <xuan.nguyen@gft.com>
2020-11-16 19:39:59 +0100

    change teamcity code 1

--- .teamcity/Parent_App2/buildTypes/Parent_App2_Build.kt
+++ .teamcity/Parent_App2/buildTypes/Parent_App2_Build.kt
@@ -5,7 +5,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 
 object Parent_App2_Build : BuildType({
     uuid = "b8a67d62-1718-4c25-8661-52ff74293c58"
-    name = "Build"
+    name = "clean compile"
 
     vcs {
         root(Parent_App2.vcsRoots.Parent_App2_SourceApp2)
@@ -13,7 +13,7 @@ object Parent_App2_Build : BuildType({
 
     steps {
         maven {
-            goals = "clean test"
+            goals = "clean compile"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
     }
